2e3ceb68c9178ce379583edec4dd63cbcfefe13b
Noodee39 <140926790+Noodee39@users.noreply.github.com>
2023-07-30 10:31:59 +0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+
+$(".info-item .btn").click(function(){
+    $(".container").toggleClass("log-in");
+  });
+  $(".container-form .btn").$(".info-item .btn").click(function(){
+    $(".container").toggleClass("log-in");
+  });
+  $(".container-form .btn").click(function(){
+    $(".container").addClass("active");
+  })click(function(){
+    $(".container").addClass("active");
+  })
